1940298d576f196cc09b7a8980969e1e761bfd9d
justforlxz <justforlxz@gmail.com>
2022-08-03 10:15:24 +0800

    feat: add chinese

--- src/contribute/contribute.service.ts
+++ src/contribute/contribute.service.ts
@@ -14,6 +14,67 @@ export class ContributeService {
     private readonly axios: HttpService,
     private readonly settings: SettingsService,
   ) {}
+  english() {
+    let markdown = `This page records summary statistics on the number of contributions deepin has made to upstream. The corresponding contributions have been submitted and merged into the upstream codebase.\n`;
+    markdown += `\nNote: Contributions submitted upstream do not always use the email of the deepin or uniontech domains.\n`;
+    markdown += `| Name | Contributions | Repository |\n`;
+    markdown += `| --- | --- | --- |\n`;
+    return markdown;
+  }
+  chinese() {
+    let markdown = `&#x6B64;&#x9875;&#x9762;&#x5C55;&#x793A;&#x4E86; deepin &#x5BF9;&#x4E0A;&#x6E38;&#x6E90;&#x7801;&#x505A;&#x51FA;&#x7684;&#x8D21;&#x732E;&#x7684;&#x6570;&#x91CF;&#x7EDF;&#x8BA1;&#x3002;&#x4E0E;&#x4E4B;&#x5BF9;&#x5E94;&#x7684;&#x8D21;&#x732E;&#x5747;&#x5DF2;&#x63D0;&#x4EA4;&#x5E76;&#x5408;&#x5165;&#x5230;&#x4E0A;&#x6E38;&#x4EE3;&#x7801;&#x4ED3;&#x5E93;&#x4E2D;&#x3002;\n`;
+    markdown += `\n&#x6CE8;&#xFF1A;&#x5BF9;&#x4E0A;&#x6E38;&#x7684;&#x4EE3;&#x7801;&#x8D21;&#x732E;&#x6709;&#x53EF;&#x80FD;&#x4F7F;&#x7528;&#x4E86; deepin &#x6216; uniontech &#x4E4B;&#x5916;&#x57DF;&#x540D;&#x7684;&#x90AE;&#x7BB1;&#x3002;\n`;
+    markdown += `| &#x9879;&#x76EE; | &#x8D21;&#x732E;&#x6570;&#x91CF; | &#x4ED3;&#x5E93;&#x5730;&#x5740; |\n`;
+    markdown += `| --- | --- | --- |\n`;
+    return markdown;
+  }
+  async upload(filename: string, body: string) {
+    const context = {
+      owner: this.settings.config.github.owner,
+      repo: 'developer-center',
+    };
+
+    const octokit = new Octokit({
+      auth: await GetToken(this.settings.appInfo(), context),
+    });
+    const path = `upstream-contributions/${filename}`;
+    const branch = `master`;
+    try {
+      let contentRef: string = undefined;
+      try {
+        const content = (
+          await octokit.repos.getContent({
+            ...context,
+            path,
+          })
+        ).data as {
+          sha: string;
+        };
+        contentRef = content.sha;
+      } catch (err) {}
+
+      await octokit.repos.createOrUpdateFileContents({
+        ...context,
+        path,
+        message: 'update upstream-contributions',
+        content: encode(body), // base64
+        branch,
+        committer: {
+          name: 'justforlxz',
+          email: 'justforlxz@gmail.com',
+        },
+        author: {
+          name: 'justforlxz',
+          email: 'justforlxz@gmail.com',
+        },
+        sha: contentRef,
+      });
+    } catch (e) {
+      throw new Error(e);
+    }
+
+    return;
+  }
   async update() {
     const result = await firstValueFrom(
       this.axios.post<Root>(
@@ -63,68 +124,30 @@ export class ContributeService {
       }
       infos.set(repo, v);
     }
-    let markdown = `This page records summary statistics on the number of contributions deepin has made to upstream. The corresponding contributions have been submitted to the upstream.\n`;
-    markdown += `\n Note: Contributions submitted upstream do not always use the email of the deepin or uniontech domains, and contributions that are not merged in will be counted as well.\n`;
-    markdown += `| Name | Contributions | Repository |\n`;
-    markdown += `| --- | --- | --- |\n`;
     const a: Info[] = [];
     for (const [key, value] of infos) {
       a.push(value);
     }
     a.sort((a, b) => a.repo.localeCompare(b.repo));
 
+    let chinese = this.chinese();
+    let english = this.english();
+
     for (const value of a) {
       const line = `| ${value.repo} | ${value.addition + value.deletion} | ${
         value.url
       } |\n`;
-      markdown += line;
+      chinese += line;
+      english += line;
     }
 
-    const context = {
-      owner: this.settings.config.github.owner,
-      repo: 'developer-center',
-    };
-
-    const octokit = new Octokit({
-      auth: await GetToken(this.settings.appInfo(), context),
-    });
-    const path = `upstream-contributions/README.md`;
-    const branch = `master`;
     try {
-      let contentRef: string = undefined;
-      try {
-        const content = (
-          await octokit.repos.getContent({
-            ...context,
-            path,
-          })
-        ).data as {
-          sha: string;
-        };
-        contentRef = content.sha;
-      } catch (err) {
-        console.error(err);
-      }
-
-      await octokit.repos.createOrUpdateFileContents({
-        ...context,
-        path,
-        message: 'update upstream-contributions',
-        content: encode(markdown), // base64
-        branch,
-        committer: {
-          name: 'justforlxz',
-          email: 'justforlxz@gmail.com',
-        },
-        author: {
-          name: 'justforlxz',
-          email: 'justforlxz@gmail.com',
-        },
-        sha: contentRef,
-      });
-    } catch (e) {
-      throw new Error(e);
+      await this.upload('README.md', english);
+      await this.upload('README_zh.md', chinese);
+    } catch (err) {
+      throw new Error(err);
     }
+
     return;
   }
 }
